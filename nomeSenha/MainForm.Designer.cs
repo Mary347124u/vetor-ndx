760877078f57d6fe83f2458d59ff3c650b6df54d
Mary <marybsa135@gmail.com>
2021-04-07 14:49:59 -0300

    first commit

---
+++ nomeSenha/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DELL
+ * Date: 23/03/2021
+ * Time: 14:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace nomeSenha
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.ListBox listBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.BackColor = System.Drawing.Color.OldLace;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(53, 23);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(157, 186);
+			this.listBox1.TabIndex = 0;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.LightCoral;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.listBox1);
+			this.Name = "MainForm";
+			this.Text = "nomeSenha";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ nomeSenha/MainForm.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DELL
+ * Date: 23/03/2021
+ * Time: 14:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace nomeSenha
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			  
+			InitializeComponent();
+			 {
+            int[] ndx = { 5, 0, 3, 1, 6, 2, 4 };
+            string[] n = { "Antonio", "Carlos", "Helena", "Ariane", "Heloisa", "Antenor", "Catharine" };
+            string[] s = { "73195555", "557711", "01410", "979491", "74123698", "40100104", "84267931" };
+            for (int i=0; i<7; i++)
+            {
+                listBox1.Items.Add(n[ndx[i]]);
+                listBox1.Items.Add(s[ndx[i]]);
+            }
+        }
+			
+            
+            
+		}
+	}
+}
